d0bd396721581f6850c0df92738a8eef81737719
Andrew&&Frankie <Andrew&&Frankie@aa.io>
2016-01-15 14:53:43 -0500

    Create followToggle#render and start followToggle#handleClick.

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -1,13 +1,35 @@
 (function () {
 
 var FollowToggle = function (el) {
-  this.userId = el.userId;
-  if (el.initialFollowState === true) {
+  this.$el = $(el);
+  this.userId = this.$el.attr("data-user-id");
+  if (this.$el.attr("data-follow-state") === true) {
     this.followState = "followed";
   } else {
     this.followState = "unfollowed";
   }
-  this.$el = $(el);
+  this.render();
+};
+
+FollowToggle.prototype.render = function () {
+  if (this.followState === "followed") {
+    this.$el.html("Unfollow");
+  } else {
+    this.$el.html("Follow!");
+  }
+};
+
+FollowToggle.prototype.handleClick = function (e) {
+  e.preventDefault();
+  var method = (this.followState === "followed") ? "DELETE" : "POST";
+  var followState = this.followState;
+  $.ajax({
+    type: method,
+    url: "/users/" + this.userId + "/follow",
+    success: function () {
+
+    },
+  });
 };
 
 module.exports = FollowToggle;
